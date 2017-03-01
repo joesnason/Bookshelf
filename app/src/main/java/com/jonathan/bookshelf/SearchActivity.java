abd3610f3ea99a4a977cead7820e8d934a0ea657
joesnason <Jonathan_hsieh@moremote.com>
2017-03-01 17:19:51 +0800

    add SearchView

--- app/src/main/java/com/jonathan/bookshelf/SearchActivity.java
+++ app/src/main/java/com/jonathan/bookshelf/SearchActivity.java
@@ -1,8 +1,15 @@
 package com.jonathan.bookshelf;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 
@@ -10,6 +17,7 @@ import com.jonathan.bookshelf.databases.BookDAO;
 
 public class SearchActivity extends AppCompatActivity {
 
+    private static String TAG = "SearchActivity";
     private ListView mListView;
 
     private BookDAO mBookDAO;
@@ -30,6 +38,36 @@ public class SearchActivity extends AppCompatActivity {
                 new int[] {android.R.id.text1, android.R.id.text2});
 
         mListView.setAdapter(adapter);
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater  = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+
+        MenuItem menuSearchItem  = menu.findItem(R.id.my_search);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView  = (SearchView) menuSearchItem.getActionView();
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                Log.d(TAG,"get string: " + newText);
+                return false;
+            }
+        });
+
+
+        searchView.setIconifiedByDefault(true);
+
+        return true;
     }
 }
